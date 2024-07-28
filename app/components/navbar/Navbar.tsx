376eb3a4afb8dcb10ebe72cc72814404fbab970a
Navishh <navishkanuwan11@gmail.com>
2024-07-28 14:34:28 +0530

    refactor

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import { User } from "@prisma/client";
+import { SafeUser } from "@/app/types";
 import Container from "../Container";
 import Logo from "./Logo";
 import Search from "./search";
 import UserMenu from "./UserMenu";
 
 interface NavbarProps {
-  currentUser?: User | null;
+  currentUser?: SafeUser | null;
 }
 
 const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
