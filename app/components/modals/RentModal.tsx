865807b83ba6a97949dc4bf84b297368f81fa91b
Navishh <navishkanuwan11@gmail.com>
2024-08-01 12:45:20 +0530

    feat: update rent modal

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -2,6 +2,7 @@
 
 import useRentModal from "@/app/hooks/useRentModal";
 import { useMemo, useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
 import { categories } from "../categories/allcategories";
 import Heading from "../Heading";
 import CategoryInput from "../inputs/CategoryInput";
@@ -20,6 +21,37 @@ const RentModal = () => {
   const rentModal = useRentModal();
 
   const [step, setStep] = useState(STEPS.CATEGORY);
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    formState: { errors },
+    reset,
+  } = useForm<FieldValues>({
+    defaultValues: {
+      category: "",
+      location: null,
+      guestCount: 1,
+      roomCount: 1,
+      bathroomCount: 1,
+      imageSrc: "",
+      price: 1,
+      title: "",
+      description: "",
+    },
+  });
+
+  const category = watch("category");
+
+  const setCustomValue = (id: string, value: any) => {
+    setValue(id, value, {
+      shouldValidate: true,
+      shouldDirty: true,
+      shouldTouch: true,
+    });
+  };
 
   const onBack = () => {
     setStep((value) => value - 1);
@@ -54,8 +86,8 @@ const RentModal = () => {
         {categories.map((item) => (
           <div key={item.label} className="col-span-1">
             <CategoryInput
-              onClick={() => {}}
-              selected={false}
+              onClick={(category) => setCustomValue("category", category)}
+              selected={category == item.label}
               label={item.label}
               icon={item.icon}
             />
