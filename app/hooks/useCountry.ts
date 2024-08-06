1fc0ffbe0781376448ecd76c338fbef352dd406f
Navishh <navishkanuwan11@gmail.com>
2024-08-06 11:59:03 +0530

    fix(map): location fetch

--- app/hooks/useCountry.ts
+++ app/hooks/useCountry.ts
@@ -1,3 +1,27 @@
+// import countries from "world-countries";
+
+// const formattedCountries = countries.map((country) => ({
+//   value: country.cca2,
+//   label: country.name.common,
+//   flag: `https://flagcdn.com/w40/${country.cca2.toLowerCase()}.png`, // URL to flag image
+//   latlng: country.latlng,
+//   region: country.region,
+// }));
+
+// const useCountries = () => {
+//   const getAll = () => formattedCountries;
+
+//   const getByValue = (value: string) => {
+//     return formattedCountries.find((item) => item.value == value);
+//   };
+
+//   return {
+//     getAll,
+//     getByValue,
+//   };
+// };
+
+// export default useCountries;
 import countries from "world-countries";
 
 const formattedCountries = countries.map((country) => ({
