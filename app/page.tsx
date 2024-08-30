53c28c09bfbc5f9253e71448db94003eae1c66fa
Navishh <navishkanuwan11@gmail.com>
2024-08-30 18:04:19 +0530

    feat: EmptyState.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,25 @@
-const Home = () => {
-  return <div className="text-rose-500 text-2xl"> Hello Airbnb</div>;
-};
+import ClientOnly from "./components/ClientOnly";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
 
-export default Home;
+export default function Home() {
+  const isEmpty = true;
+
+  if (isEmpty) {
+    return (
+      <ClientOnly>
+        <EmptyState />
+      </ClientOnly>
+    );
+  }
+
+  return (
+    <ClientOnly>
+      <Container>
+        <div className="grid grid-cols-1 gap-8 pt-24 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+          <div>my future listings</div>
+        </div>
+      </Container>
+    </ClientOnly>
+  );
+}
