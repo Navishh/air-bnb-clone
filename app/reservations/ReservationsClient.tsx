e9ab66cfb8a3089859190f5e0796b5e4b4c1ab33
Navishh <navishkanuwan11@gmail.com>
2024-09-08 18:12:26 +0530

    feat(favorites):create favorite trips

--- app/reservations/ReservationsClient.tsx
+++ app/reservations/ReservationsClient.tsx
@@ -1,6 +1,69 @@
 "use client";
-const ReservationsClient = () => {
-  return <div>Reservations</div>;
+
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useCallback, useState } from "react";
+import toast from "react-hot-toast";
+
+import { SafeReservation, SafeUser } from "../types";
+
+import Container from "../components/Container";
+import Heading from "../components/Heading";
+import ListingCard from "../components/listings/ListingCard";
+
+interface ReservationClientProps {
+  reservations: SafeReservation[];
+  currentUser: SafeUser | null;
+}
+
+const ReservationsClient: React.FC<ReservationClientProps> = ({
+  reservations,
+  currentUser,
+}) => {
+  const router = useRouter();
+
+  const [deletingId, setDeletingId] = useState("");
+
+  const onCancel = useCallback(
+    (id: string) => {
+      setDeletingId(id);
+
+      axios
+        .delete(`/api/reservations/${id}`)
+        .then(() => {
+          toast.success("reservation cancelled!");
+          router.refresh();
+        })
+        .catch(() => {
+          toast.error("Something went wrong");
+        })
+        .finally(() => {
+          setDeletingId("");
+        });
+    },
+    [router]
+  );
+
+  return (
+    <Container>
+      <Heading title="Reservations" subtitle="Bookings on your properties" />
+
+      <div className="grid grid-cols-1 gap-8 mt-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+        {reservations.map((reservation) => (
+          <ListingCard
+            key={reservation.id}
+            data={reservation.listing}
+            reservation={reservation}
+            actionId={reservation.id}
+            onAction={onCancel}
+            disabled={deletingId === reservation.id}
+            actionLabel="Cancel guest reservation"
+            currentUser={currentUser}
+          />
+        ))}
+      </div>
+    </Container>
+  );
 };
 
 export default ReservationsClient;
--- app/trips/page.tsx
+++ app/trips/page.tsx
@@ -39,3 +39,126 @@ const TripsPage = async () => {
 };
 
 export default TripsPage;
+
+// import getCurrentUser from "../actions/getCurrentUser";
+// import getReservations from "../actions/getReservations";
+// import ClientOnly from "../components/ClientOnly";
+// import EmptyState from "../components/EmptyState";
+// import TripsClient from "./TripsClient";
+
+// const TripsPage = async () => {
+//   const currentUser = await getCurrentUser();
+
+//   if (!currentUser) {
+//     return (
+//       <ClientOnly>
+//         <EmptyState title="Unauthorized" subtitle="Please login!" />
+//       </ClientOnly>
+//     );
+//   }
+
+//   const reservations = await getReservations({
+//     userId: currentUser.id,
+//   });
+
+//   if (reservations?.length === 0) {
+//     return (
+//       <ClientOnly>
+//         <EmptyState
+//           title="No Trips"
+//           subtitle="Looks like you haven't reserved any trips"
+//         />
+//       </ClientOnly>
+//     );
+//   }
+
+//   return (
+//     <ClientOnly>
+//       <TripsClient reservations={reservations} currentUser={currentUser} />
+//     </ClientOnly>
+//   );
+// };
+
+// export default TripsPage;
+
+// import getCurrentUser from "../actions/getCurrentUser";
+// import getReservations from "../actions/getReservations";
+// import ClientOnly from "../components/ClientOnly";
+// import EmptyState from "../components/EmptyState";
+// import TripsClient from "./TripsClient";
+
+// const TripsPage = async () => {
+//   const currentUser = await getCurrentUser();
+
+//   if (!currentUser) {
+//     return (
+//       <ClientOnly>
+//         <EmptyState title="Unauthorized" subtitle="Please login!" />
+//       </ClientOnly>
+//     );
+//   }
+
+//   const reservations = await getReservations({
+//     userId: currentUser.id,
+//   });
+
+//   if (reservations?.length === 0) {
+//     return (
+//       <ClientOnly>
+//         <EmptyState
+//           title="No Trips"
+//           subtitle="Looks like you haven't reserved any trips"
+//         />
+//       </ClientOnly>
+//     );
+//   }
+
+//   return (
+//     <ClientOnly>
+//       <TripsClient reservations={reservations} currentUser={currentUser} />
+//     </ClientOnly>
+//   );
+// };
+
+// export default TripsPage;
+
+// import getCurrentUser from "../actions/getCurrentUser";
+// import getReservations from "../actions/getReservations";
+// import ClientOnly from "../components/ClientOnly";
+// import EmptyState from "../components/EmptyState";
+// import TripsClient from "./TripsClient";
+
+// const TripsPage = async () => {
+//   const currentUser = await getCurrentUser();
+
+//   if (!currentUser) {
+//     return (
+//       <ClientOnly>
+//         <EmptyState title="Unauthorized" subtitle="Please login!" />
+//       </ClientOnly>
+//     );
+//   }
+
+//   const reservations = await getReservations({
+//     userId: currentUser.id,
+//   });
+
+//   if (reservations?.length === 0) {
+//     return (
+//       <ClientOnly>
+//         <EmptyState
+//           title="No Trips"
+//           subtitle="Looks like you haven't reserved any trips"
+//         />
+//       </ClientOnly>
+//     );
+//   }
+
+//   return (
+//     <ClientOnly>
+//       <TripsClient reservations={reservations} currentUser={currentUser} />
+//     </ClientOnly>
+//   );
+// };
+
+// export default TripsPage;
